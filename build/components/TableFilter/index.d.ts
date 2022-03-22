4145fc8b092bbabac2d8980106c9ddc37985e3c5
Aris Faria Muhammadun <aris.faria@accelbyte.net>
2022-03-22 14:33:33 +0700

    feat: add tooltip prop to NoResultTip and TableFilter

--- build/components/TableFilter/index.d.ts
+++ build/components/TableFilter/index.d.ts
@@ -6,8 +6,9 @@ export interface TableFilterProps {
     options: SelectOption[];
     currentValue: SelectOption;
     label?: string;
+    tooltip?: string;
     isLarge?: boolean;
     isDisabled?: boolean;
     customStyle?: React.CSSProperties;
 }
-export declare const TableFilter: ({ options, currentValue, onFilterChange, label, isLarge, isDisabled, customStyle, }: TableFilterProps) => JSX.Element;
+export declare const TableFilter: ({ options, currentValue, onFilterChange, label, tooltip, isLarge, isDisabled, customStyle, }: TableFilterProps) => JSX.Element;
