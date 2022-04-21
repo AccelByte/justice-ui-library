afbd2f319300eed8d69719c0c67305964f48d3a3
yanuar <yanuar@accelbyte.net>
2022-04-21 16:07:23 +0700

    feat: Add ValidSelectAsync component

---
+++ src/components/ValidatedInput/ValidSelectAsync.tsx
@@ -0,0 +1,40 @@
+/*
+ *
+ *  * Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+ *  * This is licensed software from AccelByte Inc, for limitations
+ *  * and restrictions contact your company contract manager.
+ *
+ */
+
+import * as React from "react";
+import { default as classNames } from "classnames";
+import { FieldErrorMessage, FieldLabel } from "../Form/utility";
+import { AsyncPaginate, AsyncPaginateProps } from "react-select-async-paginate";
+
+export interface ValidSelectAsyncProps<OptionType, Group, Additional, isMulti extends boolean>
+  extends AsyncPaginateProps<OptionType, Group, Additional, isMulti> {
+  label?: string;
+  errMessage?: string;
+  optionalLabel?: string;
+  isInvalid?: boolean;
+  isRequired?: boolean;
+  tooltip?: string;
+  className?: string;
+}
+
+export const ValidSelectAsync = <OptionType, Group, Additional, isMulti extends boolean>({
+  label,
+  errMessage,
+  optionalLabel,
+  isInvalid,
+  isRequired = true,
+  tooltip,
+  className,
+  ...props
+}: ValidSelectAsyncProps<OptionType, Group, Additional, isMulti>) => (
+  <div className={classNames("valid-select-input", className)}>
+    {!!label && <FieldLabel label={label} optionalLabel={optionalLabel} isRequired={isRequired} tooltip={tooltip} />}
+    <AsyncPaginate {...props} />
+    {isInvalid && <FieldErrorMessage message={errMessage} />}
+  </div>
+);
