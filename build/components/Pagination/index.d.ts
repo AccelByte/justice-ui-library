983935de4a54bb0926e7da2b4e99fb36d4e523a1
yanuar <yanuar@accelbyte.net>
2022-06-23 08:47:49 +0700

    feat(Pagination): Add limit option props #minor

--- build/components/Pagination/index.d.ts
+++ build/components/Pagination/index.d.ts
@@ -1,13 +1,20 @@
 import * as React from "react";
+import { SelectOption } from "../../types";
 import "./index.scss";
+interface PaginationLimit {
+    value: SelectOption;
+    options: SelectOption[];
+    onChange: (option: SelectOption) => void;
+}
 export interface PaginationProps {
     paging: {
         previous?: string;
         next?: string;
     };
     changePage: (page?: string) => void;
+    limit?: PaginationLimit;
     prevDataQa?: string | null;
     nextDataQa?: string | null;
 }
-declare const pagination: React.FunctionComponent<PaginationProps>;
-export { pagination as Pagination };
+declare const Pagination: React.FunctionComponent<PaginationProps>;
+export { Pagination };
