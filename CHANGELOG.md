413f84bae558bce612d228fa116c2035344cfcc1
Hamdan Hanafi <hamdan@accelbyte.net>
2022-10-25 10:07:10 +0700

    refactor: change changelog and merge version changelog to 0.11.10

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/) a
 ## [0.11.10]
 ### Refactored
 - Change type `tel` to `float` in `type` prop of `ValidFieldText`
+- make `children` prop to be optional in `ButtonWithIcon` component
 
 ## [0.11.9]
 ### Refactored
--- build/components/ButtonWithIcon/index.d.ts
+++ build/components/ButtonWithIcon/index.d.ts
@@ -1,7 +1,8 @@
 /// <reference types="react" />
 import { ButtonProps } from "../Button";
-export interface ButtonWithIconProps extends ButtonProps {
+export interface ButtonWithIconProps extends Omit<ButtonProps, "children"> {
     buttonIcon?: string;
     iconPosition?: "left" | "right";
+    children?: React.ReactNode;
 }
 export declare const ButtonWithIcon: ({ buttonIcon, iconPosition, appearance, isDisabled, isLoading, className, ...buttonProps }: ButtonWithIconProps) => JSX.Element;
