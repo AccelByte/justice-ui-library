48c3130ba016247a1572925173765496bf6d02d7
Aris Faria Muhammadun <aris.faria@accelbyte.net>
2023-02-10 10:53:28 +0700

    feat(ScrollSpy): add scrollTarget prop #patch

--- build/react-select.esm-29861e31.js
+++
@@ -1,1 +0,0 @@
-import{m as e,S as a}from"./stateManager-845a3300.esm-4b89d3f6.js";import"react";import"./index-4bd03571.esm-18f73ce7.js";import"./index-bffd2e22.js";var r=e(a);export{r as S};
---
+++ build/react-select.esm-8e48a105.js
@@ -0,0 +1,1 @@
+import{m as a,S as r}from"./stateManager-845a3300.esm-2f95f509.js";import"react";import"./index-4bd03571.esm-38a23a83.js";import"./index-db82829b.js";var s=a(r);export{s as S};
