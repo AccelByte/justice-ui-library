ef0713c81124dfc7de3bbcae0ae7e2c87e441bcd
rayhannr <rayhan@accelbyte.net>
2023-11-17 07:56:50 +0700

    chore: remove externals from rollup config as it will break DateTimePicker #patch
    This reverts commit d97d8fb58614736366edef86bcf5ce7e39db695d.

---
+++ build/nonIterableRest-e93b6547.js
@@ -0,0 +1,1 @@
+function r(r){if(Array.isArray(r))return r}function t(){throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.")}export{r as _,t as a};
